d5ac42fbb9ca3c4731667149003d49211d1cd985
Thomascogez <thomas.cogez-allix@etu.univ-lehavre.fr>
2020-02-11 10:50:05 +0100

    Updates

---
+++ precache-manifest.4295cb43f360dcc434f3d0db078b56b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80468361468dbd745a4f455b066aca81",
+    "url": "/colisweb_exercice/index.html"
+  },
+  {
+    "revision": "21d68a01b120781fd002",
+    "url": "/colisweb_exercice/static/css/2.95afd4a9.chunk.css"
+  },
+  {
+    "revision": "f6f0784a38569982ca60",
+    "url": "/colisweb_exercice/static/css/main.02c44a7a.chunk.css"
+  },
+  {
+    "revision": "21d68a01b120781fd002",
+    "url": "/colisweb_exercice/static/js/2.c4675af6.chunk.js"
+  },
+  {
+    "revision": "c5471ab9fc9857e5b01deb96d07f2cce",
+    "url": "/colisweb_exercice/static/js/2.c4675af6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6f0784a38569982ca60",
+    "url": "/colisweb_exercice/static/js/main.d634b876.chunk.js"
+  },
+  {
+    "revision": "2c2a37228e8187bc03ca",
+    "url": "/colisweb_exercice/static/js/runtime-main.f32a7397.js"
+  },
+  {
+    "revision": "a88f77554bd97c8632e77132d37d6e8e",
+    "url": "/colisweb_exercice/static/media/search-24px.a88f7755.svg"
+  }
+]);
